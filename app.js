d317a40c86267b46f01e32f0c6636f1d339550ed
aurinie <nie.sr30@gmail.com>
2024-10-27 19:06:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById("generateBtn").addEventListener("click", generateJoke);
 
+// Función asincrónica que realiza la solicitud a la API de chistes y obtiene un chiste
 async function generateJoke() {
     const url = "https://v2.jokeapi.dev/joke/Any";  // URL de la JokeAPI
     const jokeText = document.getElementById("jokeText");
@@ -9,18 +10,21 @@ async function generateJoke() {
     try {
         console.log("Realizando la solicitud a la API...");
         
+        // Se hizo la solicitud a la API usando fetch
         const response = await fetch(url);
 
         console.log("Respuesta de la API recibida:", response);
 
+        // Verificación de la respuesta de la API para asegurar que no haya errores
         if (!response.ok) {
             console.error("Error en la respuesta de la API:", response.status);
             throw new Error("Ocurrió un error al obtener el chiste.");
         }
-
+        // Se transforman los datos de la respuesta en formato JSON
         const data = await response.json();
         console.log("Datos del chiste recibidos:", data);
 
+        // Se acumula la función displayJoke para mostrar el chiste en pantalla
         displayJoke(data);
     } catch (error) {
         console.error("Error en la solicitud:", error);
@@ -32,12 +36,12 @@ async function generateJoke() {
 function displayJoke(jokeData) {
     const jokeText = document.getElementById("jokeText");
 
-    // Verificar si el chiste es de una sola parte o de dos partes (setup y delivery)
+      // Verificación de si el chiste es de una sola parte o de dos partes (setup y delivery)
     if (jokeData.type === "single") {
         jokeText.textContent = `Chiste: ${jokeData.joke}`;
     } else if (jokeData.type === "twopart") {
         jokeText.innerHTML = `<strong>${jokeData.setup}</strong><br>${jokeData.delivery}`;
     }
     
-    console.log("Chiste mostrado en pantalla:", jokeData);
+    console.log("El duende cortes", jokeData);
 }
